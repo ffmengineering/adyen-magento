a2abfd68a470e3b4cbbce6671f2eb1388c89a07c
Sander Mangel <sander@sandermangel.nl>
2017-05-03 13:06:24 +0200

    Capture invoices when zero auth is used and capture is manual. This cornjob will capture invoices that are scheduled to be paid on a crtain date

--- app/code/community/Adyen/Payment/Model/Cronjob.php
+++ app/code/community/Adyen/Payment/Model/Cronjob.php
@@ -47,6 +47,57 @@ class Adyen_Payment_Model_Cronjob {
     }
 
     /**
+     * If ZeroAuth is used and capture is manual this cronjob captures
+     * pending invoices based on the field that was used to determine
+     * capture should be postponed.
+     *
+     * @return bool
+     */
+    public function capturePendingInvoicesByDate()
+    {
+        $useZeroAuth = (bool)Mage::helper('adyen')->getConfigData('use_zero_auth');
+        $zeroAuthDateField = (bool)Mage::helper('adyen')->getConfigData('base_zero_auth_on_date');
+        $isManual = (bool)Mage::helper('adyen')->getConfigData('capture_mode');
+
+        if (!$useZeroAuth || $isManual !== 'manual') { // not used
+            return;
+        }
+
+        $collection = Mage::getResourceModel('sales/order_invoice_collection')
+            ->addAttributeToFilter('main_table.state', 1);
+
+        $collection->getSelect()->joinLeft(
+            'sales_flat_order',
+            'main_table.order_id = sales_flat_order.entity_id',
+            ['scheduled_at']
+        );
+
+        $collection->addAttributeToFilter('sales_flat_order.' . $zeroAuthDateField, ['lteq' => date('Y-m-d') . ' 23:59:59']);
+        $collection->addAttributeToFilter('sales_flat_order.state', ['in' => ['processing','payment_pending','payment_review','pending_payment']]);
+
+        foreach ($collection as $invoice) {
+            if ($invoice->canCapture()) {
+                try {
+                    $invoice->capture();
+
+                    $invoice->getOrder()->setIsInProcess(true);
+
+                    Mage::getModel('core/resource_transaction')
+                        ->addObject($invoice)
+                        ->addObject($invoice->getOrder())
+                        ->save();
+                } catch (\Exception $e) {
+                    Mage::log("{$e}", Zend_Log::ERROR, 'adyen_capture_cron.log');
+                }
+            } else {
+                Mage::log("{$invoice->getIncrementId()} up for capture, canCapture returned false", Zend_Log::WARN, 'adyen_capture_cron.log');
+            }
+        }
+
+        return true;
+    }
+
+    /**
      * Log debug data to file
      *
      * @param $storeId
